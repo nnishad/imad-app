26810f3aa953bab1defe0a57bbd00f354b8bdc73
nnishad <30749859+nnishad@users.noreply.github.com>
2017-08-07 11:12:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+// move madi
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick =function(){
+    var interval=setInterval(moveRight,100);
+};
