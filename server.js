a46a7cece2f9d427af8c31b3b25e334a0fd77116
nnishad <30749859+nnishad@users.noreply.github.com>
2017-08-07 12:00:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  counter = counter+1;
+    res.send(counter.toString());
   
 });
 
 var counter =0;
 app.get('/',function (req,res){
     counter = counter+1;
+    res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
