6009700905b07c44dc9b3787bacd85f2b4a0e1a5
nnishad <30749859+nnishad@users.noreply.github.com>
2017-08-07 12:13:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,13 @@ function moveBottom(){
     marginTop=marginTop+1;
     img.style.marginTop=marginTop+'px';
 }
+var counter=0;
 img.onclick =function(){
     var interval=setInterval(moveBottom,50);
+};
+var button =document.getElementById('cpounter');
+button.onclick=function(){
+  counter =counter+1;
+  var span =document.getElementById('count');
+  span.innerHTML=counter.toString();
 };
